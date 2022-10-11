842bfb42f9a184f3743e72341a2df80b009c955c
emreakturk1125 <akturk_emre1125@hotmail.com>
2022-10-11 21:00:39 +0300

    Added CommentController

---
+++ EA.BlogProject/EA.BlogProject.WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using NToastNotify.Helpers;
+using EA.BlogProject.Entities.Concrete;
+using EA.BlogProject.Entities.Dtos;
+using EA.BlogProject.WebUI.Areas.Admin.Models;
+using EA.BlogProject.Mvc.Helpers.Abstract;
+using EA.BlogProject.Services.Abstract;
+using EA.BlogProject.Shared.Utilities.Extensions;
+using EA.BlogProject.Shared.Utilities.Results.ComplexTypes;
+using EA.BlogProject.WebUI.Areas.Admin.Controllers;
+using EA.BlogProject.Mvc.Areas.Admin.Models;
+
+namespace EA.BlogProject.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : BaseController
+    {
+        private readonly ICommentService _commentService;
+        public CommentController(UserManager<User> userManager, IMapper mapper, IImageHelper imageHelper, ICommentService commentService) : base(userManager, mapper, imageHelper)
+        {
+            _commentService = commentService;
+        }
+        [Authorize(Roles = "Admin,Comment.Read")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _commentService.GetAllByNonDeletedAsync();
+            return View(result.Data);
+        }
+        [Authorize(Roles = "Admin,Comment.Read")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllComments()
+        {
+            var result = await _commentService.GetAllByNonDeletedAsync();
+            var commentsResult = JsonSerializer.Serialize(result, new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve,
+            });
+            return Json(commentsResult);
+        }
+        [Authorize(Roles = "Admin,Comment.Read")]
+        [HttpGet]
+        public async Task<IActionResult> GetDetail(int commentId)
+        {
+            var result = await _commentService.GetAsync(commentId);
+            if (result.ResultStatus==ResultStatus.Success)
+            {
+                return PartialView("_CommentDetailPartial", result.Data);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [Authorize(Roles = "Admin,Comment.Delete")]
+        [HttpPost]
+        public async Task<IActionResult> Delete(int commentId)
+        {
+            var result = await _commentService.DeleteAsync(commentId, LoggedInUser.UserName);
+            var commentResult = JsonSerializer.Serialize(result);
+            return Json(commentResult);
+        }
+        [Authorize(Roles = "Admin,Comment.Update")]
+        [HttpPost]
+        public async Task<IActionResult> Approve(int commentId)
+        {
+            var result = await _commentService.ApproveAsync(commentId, LoggedInUser.UserName);
+            var commentResult = JsonSerializer.Serialize(result, new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+            return Json(commentResult);
+        }
+        [Authorize(Roles = "Admin,Comment.Update")]
+        [HttpGet]
+        public async Task<IActionResult> Update(int commentId)
+        {
+            var result = await _commentService.GetCommentUpdateDtoAsync(commentId);
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return PartialView("_CommentUpdatePartial", result.Data);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [Authorize(Roles = "Admin,Comment.Update")]
+        [HttpPost]
+        public async Task<IActionResult> Update(CommentUpdateDto commentUpdateDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _commentService.UpdateAsync(commentUpdateDto, LoggedInUser.UserName);
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var commentUpdateAjaxModel = JsonSerializer.Serialize(new CommentUpdateAjaxViewModel
+                    {
+                        CommentDto = result.Data,
+                        CommentUpdatePartial = await this.RenderViewToStringAsync("_CommentUpdatePartial", commentUpdateDto)
+                    }, new JsonSerializerOptions
+                    {
+                        ReferenceHandler = ReferenceHandler.Preserve
+                    });
+                    return Json(commentUpdateAjaxModel);
+                }
+            }
+            var commentUpdateAjaxErrorModel = JsonSerializer.Serialize(new CommentUpdateAjaxViewModel
+            {
+                CommentUpdatePartial = await this.RenderViewToStringAsync("_CommentUpdatePartial", commentUpdateDto)
+            });
+            return Json(commentUpdateAjaxErrorModel);
+        }
+        [Authorize(Roles = "Admin,Comment.Read")]
+        [HttpGet]
+        public async Task<IActionResult> DeletedComments()
+        {
+            var result = await _commentService.GetAllByDeletedAsync();
+            return View(result.Data);
+
+        }
+        [Authorize(Roles = "Admin,Comment.Read")]
+        [HttpGet]
+        public async Task<JsonResult> GetAllDeletedComments()
+        {
+            var result = await _commentService.GetAllByDeletedAsync();
+            var comments = JsonSerializer.Serialize(result, new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+            return Json(comments);
+        }
+        [Authorize(Roles = "Admin,Comment.Update")]
+        [HttpPost]
+        public async Task<JsonResult> UndoDelete(int commentId)
+        {
+            var result = await _commentService.UndoDeleteAsync(commentId, LoggedInUser.UserName);
+            var undoDeleteCommentResult = JsonSerializer.Serialize(result);
+            return Json(undoDeleteCommentResult);
+        }
+        [Authorize(Roles = "Admin,Comment.Delete")]
+        [HttpPost]
+        public async Task<JsonResult> HardDelete(int commentId)
+        {
+            var result = await _commentService.HardDeleteAsync(commentId);
+            var hardDeletedCommentResult = JsonSerializer.Serialize(result);
+            return Json(hardDeletedCommentResult);
+        }
+
+    }
+}
---
+++ EA.BlogProject/EA.BlogProject.WebUI/Areas/Admin/Models/CommentUpdateAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EA.BlogProject.Entities.Dtos;
+
+namespace EA.BlogProject.Mvc.Areas.Admin.Models
+{
+    public class CommentUpdateAjaxViewModel
+    {
+        public CommentUpdateDto CommentUpdateDto { get; set; }
+        public string CommentUpdatePartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
