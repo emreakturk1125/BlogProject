e2e647fd479ff6c05a2f69aebabf4db805a91011
emreakturk1125 <akturk_emre1125@hotmail.com>
2022-11-27 20:49:11 +0300

    appsettings.json development

--- EA.BlogProject/EA.BlogProject.WebUI/Program.cs
+++ EA.BlogProject/EA.BlogProject.WebUI/Program.cs
@@ -18,13 +18,25 @@ namespace EA.BlogProject.WebUI
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.Sources.Clear();
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
+                if (args != null)
+                {
+                    config.AddCommandLine(args);
+                }
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                }).ConfigureLogging(logging =>{
-
-                    logging.ClearProviders();    // Diğer loggerları disactive etmek gerekiyor
+                }).ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders(); // Diğer loggerları disactive etmek gerekiyor
                 }).UseNLog();
+      
     }
 }
